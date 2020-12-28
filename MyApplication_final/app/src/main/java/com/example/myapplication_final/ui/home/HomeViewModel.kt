0968b3e22d2b4003f9390834560514d0a91e3b11
zhangerhh <71538147+zhangerhh@users.noreply.github.com>
2020-12-28 13:31:28 +0800

    第六次提交

--- MyApplication_final/app/src/main/java/com/example/myapplication_final/ui/home/HomeViewModel.kt
+++ MyApplication_final/app/src/main/java/com/example/myapplication_final/ui/home/HomeViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class HomeViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+        value = "Welcome to APP"
     }
     val text: LiveData<String> = _text
 }
